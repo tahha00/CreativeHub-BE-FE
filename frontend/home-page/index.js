7a3e6f9622e8b7c4c6c635fa0fa24918944dc94f
“yourGithubUsername” <your@emailadd.com>
2023-11-27 13:32:12 +0000

    Checks for empty string and shows dropdown menu

--- frontend/home-page/index.js
+++ frontend/home-page/index.js
@@ -1,5 +1,47 @@
+const list = document.getElementById('list');
 
 
+
+
+/* When the user clicks on the button,
+ shows the dropdown content */
+function myFunction() {
+    
+    if (list.style.display === 'none' || list.style.display === '') {
+        list.style.display = 'block';
+      } else {
+        list.style.display = 'none';
+      };
+  }
+  //code to filter dropdown list items based on user input
+  function filterFunction() {
+    let input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("searchFilter");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+      txtValue = a[i].textContent || a[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+      } else {
+        a[i].style.display = "none";
+      }
+    }
+  }
+
+
+
+const showDropdown = document.querySelector("#myInput")
+showDropdown.addEventListener("click", myFunction);
+
+
+
+
+
+
+
+//This code initializes the date range picker using the jQuery library, allowing users to select date ranges.
 $(function() {
     $('input[name="daterange"]').daterangepicker({
       opens: 'left'
@@ -7,3 +49,4 @@ $(function() {
       console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
   });
+
